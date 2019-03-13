cec3bbb8ebf11778c3e1642bcc23f89aa3445a1c
ykonno <konchan1129@gmail.com>
2019-03-13 12:16:39 +0900

    set status code.

--- src/main/java/com/example/user/config/ErrorHandler.java
+++ src/main/java/com/example/user/config/ErrorHandler.java
@@ -1,11 +1,13 @@
 package com.example.user.config;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * @author ykonno.
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ErrorHandler implements ErrorController {
 
     @RequestMapping("/error")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public String error404(Model model, @AuthenticationPrincipal UserDetails user){
         model.addAttribute("user", user.getUsername());
 
